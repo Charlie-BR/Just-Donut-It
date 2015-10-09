20124ada178e00d07e017a6fba5d8c2e44babd22
Charles Bittner-Rossmiller <charles.bittnerrossmiller@gmail.com>
2015-10-09 15:32:01 -0500

    added google maps, it works now to add to the list, MVP

--- js/app.js
+++ js/app.js
@@ -1,37 +1,36 @@
 $(function() {
 
     var donut_api = "http://api.doughnuts.ga/doughnuts"
-    var flavor_val = $('#doughnut-flavor').val();
-    var style_val = $('#doughnut-style').val();
 
-    $('.button-submit').on('click', function () { 
-      $.ajax({
-    	    url: donut_api
-    		dataType: 'jsonp',
-            method: 'POST',
-    		},
-            
-            success: function(data) {
-              $("#doughnuts").append("<li>"+ flavor_val +" which is a " style_val "donut"+"</li>")
-            }
-    		
-            fail: function(err) {
-    			console.log('fail!');
-    			console.log(err);
-    		},
-      });
-    }); //
-
-    $.ajax({
-            url: donut_api
+        $.ajax({
+            url: donut_api,
             dataType: 'jsonp',
             method: 'GET',
             
             success: function(data) {
+                console.log(data);
             for (var i=0; i < data.length; i++) {
-              $("#doughnuts").append("<li>"+ data[i]['id']+data[i]['flavor']+data[i]['style']+"</li>")
+              $("#doughnuts").append("<li>"+ data[i]['flavor']+" | "+data[i]['style']+"</li>")
                 }
-            }
+            },
     });
+
+    $('.button-submit').on('click', function () { 
+        var flavor_val = $('#doughnut-flavor').val();
+        var style_val = $('#doughnut-style').val();
+      
+      $.ajax({
+    	    url: donut_api,
+    		dataType: 'jsonp',
+            method: 'POST',
+    		
+            success: function(data) {
+              $("#doughnuts").append("<li>"+ flavor_val +" | " +style_val+"</li>");
+            }
+    		
+      });
+    }); //
+
+
 });    
 
